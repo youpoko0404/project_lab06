9c658511121f696cfea27322b5b41695c7dc87c7
youpoko0404 <clan.sf123@gmail.com>
2020-09-23 16:54:06 +0700

    change key api

--- src/components/api.vue
+++ src/components/api.vue
@@ -1,38 +1,43 @@
 <template>
-<body>
-  <b-container class="bv-example-row">
-    <div class="search">
-      <input type="text" class="searchTerm" v-model="textSearch" placeholder="Search" />
-      <button type="submit" class="searchButton" @click="searchData()">
-        <i>Search</i>
-      </button>
-    </div>
-  </b-container>
-  <vs-row class="mb-3" align="center" justify="space-around">
-    <vs-card class="mt-3" type="2" v-for="list in playlist" :key="list.items">
-      <template #img>
-        <img :src="list.snippet.thumbnails.high.url" />
-      </template>
-      <template #title>
-        <b-card-body :title="list.snippet.title"></b-card-body>
-      </template>
-      <template #text>
-        <template>
-          <div class="center">
-            <vs-button
-              block
-              dark
-              @click="active = 4"
-              :href="'https://www.youtube.com/watch?v=' + list.id.videoId "
-            >
-              <i class="bx bxs-paint-roll"></i>Go to Youtube
-            </vs-button>
-          </div>
+  <body>
+    <b-container class="bv-example-row">
+      <div class="search">
+        <input
+          type="text"
+          class="searchTerm"
+          v-model="textSearch"
+          placeholder="Search"
+        />
+        <button type="submit" class="searchButton" @click="searchData()">
+          <i>Search</i>
+        </button>
+      </div>
+    </b-container>
+    <vs-row class="mb-3" align="center" justify="space-around">
+      <vs-card class="mt-3" type="2" v-for="list in playlist" :key="list.items">
+        <template #img>
+          <img :src="list.snippet.thumbnails.high.url" />
         </template>
-      </template>
-    </vs-card>
-  </vs-row>
-</body>
+        <template #title>
+          <b-card-body :title="list.snippet.title"></b-card-body>
+        </template>
+        <template #text>
+          <template>
+            <div class="center">
+              <vs-button
+                block
+                dark
+                @click="active = 4"
+                :href="'https://www.youtube.com/watch?v=' + list.id.videoId"
+              >
+                <i class="bx bxs-paint-roll"></i>Go to Youtube
+              </vs-button>
+            </div>
+          </template>
+        </template>
+      </vs-card>
+    </vs-row>
+  </body>
 </template>
 
 <script>
@@ -51,9 +56,8 @@ export default {
         .get(
           "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=50&q=" +
             this.textSearch +
-            "&type=video&key=AIzaSyC_-EWAc_Ifc6GpN7cEy1jlGKLAMNatLrU"
+            "&type=video&key=AIzaSyB6ZdgM1OVHpyhLyXsrDSWcoSolZyCeoVg"
         )
-
         .then((Response) => {
           this.playlist = Response.data.items; //.slice(0, 20);
         })
